82c98f2c9c9fe230945ad2b9be41c0081725d1a6
ben <boneill@rnet-tech.com>
2023-05-03 08:28:23 -0400

    removing dockerm from this repo for clarity. It is still available, just not linked as a submodule here because it is part of the vnvlabs.com cloud deployment system and not the core toolkit

--- docker.sh
+++ docker.sh
@@ -1,44 +1,43 @@
 
-REPO_OWNER=$1
-REON=$2
+REPO_OWNER=ghcr.io/vnvlabs
+REON=$1
 
+set -e
 
+#Build the environment required to run all the applications
 cd env 
-#./docker.sh <tag>
 ./docker.sh ${REPO_OWNER}/env:${REON} 
 cd ..
 
+#Build the vnv toolkit library
 cd vnv 
-#./docker.sh <base image to build from> <tag>
 ./docker.sh ${REPO_OWNER}/env:${REON}  ${REPO_OWNER}/raw:${REON} 
 cd .. 
 
+#Add the gui to the image. This builds a heavy gui on top of the vnv image, and a light image
+#on top of ubuntu:20.04
+cd gui 
+#./docker.sh <tag>
+./docker.sh ${REPO_OWNER}/heavy_gui:${REON} ${REPO_OWNER}/raw:${REON} ${REPO_OWNER}/gui:${REON}
+cd ..
 
+#Add all of the plugins
 cd plugins 
-
-#./docker.sh <base image to build from> <tag>
- ./docker.sh ${REPO_OWNER}/raw:${REON} ${REPO_OWNER}/base:${REON} 
+ ./docker.sh ${REPO_OWNER}/heavy_gui:${REON} ${REPO_OWNER}/base:${REON} 
 cd .. 
 
+#Build all of the applications. 
 cd applications 
 #./docker.sh <base image to build from> <repo name> <version>
 ./docker.sh ${REPO_OWNER}/base:${REON} ${REPO_OWNER} ${REON} 
 cd .. 
 
-cd gui 
-#./docker.sh <tag>
-./docker.sh ${REPO_OWNER}/gui:${REON}  
-cd ..
-
-cd dockerm 
-# ./docker.sh <tag> <image to use when appending gui to containers>
-./docker.sh ${REPO_OWNER}/dockerm:${REON} ${REPO_OWNER}/gui:${REON}  
-cd ..
 
 if [[ x"${PUSH_TO_GHCR}" == "x1"  ]]; then
 
-for package in env demo proxy_apps hypre petsc mfem moose asgard all dockerm gui   
+for package in env demo proxy_apps hypre petsc mfem moose asgard all gui   
 do
+  
   docker push ${REPO_OWNER}/${package}:${REON}
 done
 
