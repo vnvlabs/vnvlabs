20c43b2945031323977b7020afeabab5e0d7c6b7
root <Ben O'Neill>
2023-12-14 16:25:22 -0500

    updates

--- build_scripts/build_moose.sh
+++ build_scripts/build_moose.sh
@@ -11,5 +11,6 @@ cd ${SOURCE_DIR}/applications/moose/framework\
     		          --output src/utils/vnv_MOOSE.C \
                       compile_commands.json \
    && make -j \
+   && python3 /vnvlabs/build_scripts/utils/run_in_subdirs.py ${SOURCE_DIR}/applications/moose/examples make vnv \
    && ${VNV_REGISTER} gui moose ${SOURCE_DIR}/applications/moose/vnv/gui/vnv.__registration__ 
 
