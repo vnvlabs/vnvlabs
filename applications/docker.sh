234c5e7469674ffcfd35e262e03d7812303600a4
Your Name <you@example.com>
2024-08-16 16:28:56 -0400

    updates

--- applications/docker.sh
+++ applications/docker.sh
@@ -2,62 +2,21 @@
 set -e 
 set -o pipefail
 
+BASE_IMG=$1
 REPO_OWNER=$2
 REON=$3
-
-#Build asgard 
-
-docker pull ${REPO_OWNER}/asgard:${REON} || echo "dne"
-docker build -f asgard/vnv/Dockerfile -t ${REPO_OWNER}/asgard:${REON} --build-arg FROM_IMAGE=$1 asgard 
-docker push ${REPO_OWNER}/asgard:${REON}
-
-#Build the heat
-
-docker pull ${REPO_OWNER}/heat:${REON} || echo "dne"
-docker build -f heat/vnv/Dockerfile -t ${REPO_OWNER}/heat:${REON} --build-arg FROM_IMAGE=$1 heat 
-docker push ${REPO_OWNER}/heat:${REON}
-#Build simple 
-
-docker pull ${REPO_OWNER}/simple:${REON} || echo "dne"
-docker build -f simple/vnv/Dockerfile -t ${REPO_OWNER}/simple:${REON} --build-arg FROM_IMAGE=$1 simple 
-docker push ${REPO_OWNER}/simple:${REON}
-##miniamr
-
-docker pull ${REPO_OWNER}/miniamr:${REON} || echo "dne"
-docker build -f miniamr/vnv/Dockerfile -t ${REPO_OWNER}/miniamr:${REON} --build-arg FROM_IMAGE=$1 miniamr 
-docker push ${REPO_OWNER}/miniamr:${REON}
-#swfft
-
-docker pull ${REPO_OWNER}/swfft:${REON} || echo "dne"
-docker build -f swfft/vnv/Dockerfile -t ${REPO_OWNER}/swfft:${REON} --build-arg FROM_IMAGE=$1 swfft
-docker push ${REPO_OWNER}/swfft:${REON}
-#hypre
-
-docker pull ${REPO_OWNER}/hypre:${REON} || echo "dne"
-docker build -f hypre/vnv/Dockerfile -t ${REPO_OWNER}/hypre:${REON} --build-arg FROM_IMAGE=$1 hypre
-docker push ${REPO_OWNER}/hypre:${REON}
-
-docker pull ${REPO_OWNER}/petsc:${REON} || echo "dne"
-docker build -f petsc/vnv/Dockerfile -t ${REPO_OWNER}/petsc:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/hypre:${REON} petsc 
-docker push ${REPO_OWNER}/petsc:${REON}
-#mfem
-
-docker pull ${REPO_OWNER}/mfem:${REON} || echo "dne"
-docker build -f mfem/vnv/Dockerfile -t ${REPO_OWNER}/mfem:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/petsc:${REON} mfem 
-docker push ${REPO_OWNER}/mfem:${REON}
-#libmesh
-
-docker pull ${REPO_OWNER}/libmesh:${REON} || echo "dne"
-docker build -f libmesh/vnv/Dockerfile -t ${REPO_OWNER}/libmesh:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/petsc:${REON} libmesh
-docker push ${REPO_OWNER}/libmesh:${REON}
-#moose
-
-docker pull ${REPO_OWNER}/moose:${REON} || echo "dne"
-docker build -f moose/vnv/Dockerfile -t ${REPO_OWNER}/moose:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/libmesh:${REON} moose 
-docker push ${REPO_OWNER}/moose:${REON}
+MODIFIED=$4
 
 
 
-
-
+check_and_build "asgard" ${BASE_IMG}
+check_and_build "heat" ${BASE_IMG}
+check_and_build "simple" ${BASE_IMG}
+check_and_build "miniamr" ${BASE_IMG}
+check_and_build "swfft" ${BASE_IMG}
+check_and_build "hypre" ${BASE_IMG}
+check_and_build "petsc" "hypre"
+check_and_build "mfem" "petsc"
+check_and_build "libmesh" "petsc"
+check_and_build "moose" "libmesh"
 
--- docker.sh
+++ docker.sh
@@ -3,45 +3,80 @@
 set -e 
 set -o pipefail
 
+function check_and_build() {
+    # Parameters
+    REPO_NAME=$1
+    BASE_IMAGE_NAME=$2
+    DONE=$3
+
+    # Get the current Git commit ID of the repository
+    GIT_COMMIT_ID=$(cd $REPO_NAME && git rev-parse HEAD)
+    BUILD_REPO=false
+
+    # 1. Check if MODIFIED is false and perform actions
+    if [ "$MODIFIED" = "no" ]; then
+        docker pull ${REPO_OWNER}/${REPO_NAME}:${REON}
+        CURRENT_COMMIT_ID=$(docker inspect --format='{{ index .Config.Labels "commit_id" }}' ${REPO_OWNER}/${REPO_NAME}:${REON} 2>/dev/null || echo "1")
+        echo $GIT_COMMIT_ID
+        echo $CURRENT_COMMIT_ID
+        if [ "$CURRENT_COMMIT_ID" != "$GIT_COMMIT_ID" ]; then
+            echo "Rebuilding as commit has changed"
+            BUILD_REPO=true
+        fi
+    else
+        BUILD_REPO=true
+    fi
+
+    # 2. Build and push the Docker image if needed
+    if [ "$BUILD_REPO" = true ]; then
+        MODIFIED="yes"
+        docker build --label commit_id=$GIT_COMMIT_ID -f ${REPO_NAME}/vnv/Dockerfile -t ${REPO_OWNER}/${REPO_NAME}:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/${BASE_IMAGE_NAME}:${REON} $REPO_NAME
+        docker push ${REPO_OWNER}/${REPO_NAME}:${REON}
+    fi
+
+    # If we are done with this image, then we can delete it
+    if [ "${DONE}" = "yes"]; then
+        docker rmi ${REPO_OWNER}/${REPO_NAME}:${REON}
+    fi
+}
+
 
 
 REPO_OWNER=ghcr.io/vnvlabs
 REON=run
+MODIFIED="no"
+LOWMEM="yes"
 
 #Build the environment required to run all the applications
 cd env 
-docker pull ${REPO_OWNER}/env:${REON} || echo "dne"
-docker build -f Dockerfile -t ${REPO_OWNER}/env:${REON} . 
-docker push ${REPO_OWNER}/env:${REON}
+docker pull ${REPO_OWNER}/env:${REON}
+GIT_COMMIT_ID=$(sha256sum Dockerfile | cut -f1 -d' ')
+CURRENT_COMMIT_ID=$(docker inspect --format='{{ index .Config.Labels "commit_id" }}' ${REPO_OWNER}/env:${REON} 2>/dev/null || echo "1")
+
+echo $GIT_COMMIT_ID
+echo $CURRENT_COMMIT_ID
+if [ "$CURRENT_COMMIT_ID" != "$GIT_COMMIT_ID" ]; then
+    docker build --label commit_id=$GIT_COMMIT_ID -f Dockerfile -t ${REPO_OWNER}/env:${REON} . 
+    docker push ${REPO_OWNER}/env:${REON}
+    MODIFIED="yes"
+fi
 cd ..
 
-#Build the gui top of ubuntu and on top of the vnv image. 
+check_and_build "gui" "env" "no"
+check_and_build "vnv" "gui" "no"
 
-cd gui 
-docker pull ${REPO_OWNER}/gui:${REON} || echo "dne"
-docker build -f docker/Dockerfile -t ${REPO_OWNER}/gui:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/env:${REON} .
-docker push ${REPO_OWNER}/gui:${REON}
+cd plugins
+check_and_build "performance" "vnv" "no"
 cd ..
 
-#Build the vnv toolkit library
-cd vnv 
-docker pull ${REPO_OWNER}/vnv:${REON} || echo "dne"
-docker build -f docker/Dockerfile --build-arg FROM_IMAGE=${REPO_OWNER}/gui:${REON} -t ${REPO_OWNER}/vnv:${REON} . 
-docker push ${REPO_OWNER}/vnv:${REON}
-cd .. 
-
-#Add all of the plugins
-cd plugins/performance
-docker pull ${REPO_OWNER}/vnvp:${REON} || echo "dne"
-docker build -f docker/Dockerfile --build-arg FROM_IMAGE=${REPO_OWNER}/vnv:${REON} -t ${REPO_OWNER}/vnvp:${REON} .  
-docker push ${REPO_OWNER}/vnvp:${REON}
-cd ../.. 
-
-
-#Build all of the applications. 
-cd applications 
-./docker.sh ${REPO_OWNER}/vnvp:${REON} ${REPO_OWNER} ${REON} 
-cd .. 
-
-
-
+cd applications
+check_and_build "asgard" "performance" ${LOWMEM}
+check_and_build "heat" "performance" ${LOWMEM}
+check_and_build "simple" "performance" ${LOWMEM}
+check_and_build "miniamr" "performance" ${LOWMEM}
+check_and_build "swfft" "performance" ${LOWMEM}
+check_and_build "hypre" "performance" "no"
+check_and_build "petsc" "hypre" "no"
+check_and_build "mfem" "petsc" ${LOWMEM}
+check_and_build "libmesh" "petsc" "no"
+check_and_build "moose" "libmesh" "yes"
