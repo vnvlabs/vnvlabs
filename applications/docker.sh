647ce27034e644ff39d19c30d6db6e1ee5248679
Your Name <you@example.com>
2024-08-16 14:33:47 -0400

    updates

--- applications/docker.sh
+++ applications/docker.sh
@@ -6,37 +6,57 @@ REPO_OWNER=$2
 REON=$3
 
 #Build asgard 
-docker build -f asgard/vnv/Dockerfile -t ${REPO_OWNER}/asgard:${REON} --build-arg FROM_IMAGE=$1 asgard  &
+
+docker pull ${REPO_OWNER}/asgard:${REON} || echo "dne"
+docker build -f asgard/vnv/Dockerfile -t ${REPO_OWNER}/asgard:${REON} --build-arg FROM_IMAGE=$1 asgard 
+docker push ${REPO_OWNER}/asgard:${REON}
 
 #Build the heat
-docker build -f heat/vnv/Dockerfile -t ${REPO_OWNER}/heat:${REON} --build-arg FROM_IMAGE=$1 heat &
 
+docker pull ${REPO_OWNER}/heat:${REON} || echo "dne"
+docker build -f heat/vnv/Dockerfile -t ${REPO_OWNER}/heat:${REON} --build-arg FROM_IMAGE=$1 heat 
+docker push ${REPO_OWNER}/heat:${REON}
 #Build simple 
-docker build -f simple/vnv/Dockerfile -t ${REPO_OWNER}/simple:${REON} --build-arg FROM_IMAGE=$1 simple &
 
+docker pull ${REPO_OWNER}/simple:${REON} || echo "dne"
+docker build -f simple/vnv/Dockerfile -t ${REPO_OWNER}/simple:${REON} --build-arg FROM_IMAGE=$1 simple 
+docker push ${REPO_OWNER}/simple:${REON}
 ##miniamr
-docker build -f miniamr/vnv/Dockerfile -t ${REPO_OWNER}/miniamr:${REON} --build-arg FROM_IMAGE=$1 miniamr &
 
+docker pull ${REPO_OWNER}/miniamr:${REON} || echo "dne"
+docker build -f miniamr/vnv/Dockerfile -t ${REPO_OWNER}/miniamr:${REON} --build-arg FROM_IMAGE=$1 miniamr 
+docker push ${REPO_OWNER}/miniamr:${REON}
 #swfft
-docker build -f swfft/vnv/Dockerfile -t ${REPO_OWNER}/swfft:${REON} --build-arg FROM_IMAGE=$1 swfft &
 
+docker pull ${REPO_OWNER}/swfft:${REON} || echo "dne"
+docker build -f swfft/vnv/Dockerfile -t ${REPO_OWNER}/swfft:${REON} --build-arg FROM_IMAGE=$1 swfft
+docker push ${REPO_OWNER}/swfft:${REON}
 #hypre
+
+docker pull ${REPO_OWNER}/hypre:${REON} || echo "dne"
 docker build -f hypre/vnv/Dockerfile -t ${REPO_OWNER}/hypre:${REON} --build-arg FROM_IMAGE=$1 hypre
+docker push ${REPO_OWNER}/hypre:${REON}
 
+docker pull ${REPO_OWNER}/petsc:${REON} || echo "dne"
 docker build -f petsc/vnv/Dockerfile -t ${REPO_OWNER}/petsc:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/hypre:${REON} petsc 
-
+docker push ${REPO_OWNER}/petsc:${REON}
 #mfem
-docker build -f mfem/vnv/Dockerfile -t ${REPO_OWNER}/mfem:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/petsc:${REON} mfem &
 
+docker pull ${REPO_OWNER}/mfem:${REON} || echo "dne"
+docker build -f mfem/vnv/Dockerfile -t ${REPO_OWNER}/mfem:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/petsc:${REON} mfem 
+docker push ${REPO_OWNER}/mfem:${REON}
 #libmesh
+
+docker pull ${REPO_OWNER}/libmesh:${REON} || echo "dne"
 docker build -f libmesh/vnv/Dockerfile -t ${REPO_OWNER}/libmesh:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/petsc:${REON} libmesh
-
+docker push ${REPO_OWNER}/libmesh:${REON}
 #moose
+
+docker pull ${REPO_OWNER}/moose:${REON} || echo "dne"
 docker build -f moose/vnv/Dockerfile -t ${REPO_OWNER}/moose:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/libmesh:${REON} moose 
+docker push ${REPO_OWNER}/moose:${REON}
 
 
-
-wait 
 
 
 
--- docker.sh
+++ docker.sh
@@ -3,28 +3,38 @@
 set -e 
 set -o pipefail
 
+
+
 REPO_OWNER=ghcr.io/vnvlabs
 REON=run
 
 #Build the environment required to run all the applications
 cd env 
+docker pull ${REPO_OWNER}/env:${REON} || echo "dne"
 docker build -f Dockerfile -t ${REPO_OWNER}/env:${REON} . 
+docker push ${REPO_OWNER}/env:${REON}
 cd ..
 
 #Build the gui top of ubuntu and on top of the vnv image. 
 
 cd gui 
+docker pull ${REPO_OWNER}/gui:${REON} || echo "dne"
 docker build -f docker/Dockerfile -t ${REPO_OWNER}/gui:${REON} --build-arg FROM_IMAGE=${REPO_OWNER}/env:${REON} .
+docker push ${REPO_OWNER}/gui:${REON}
 cd ..
 
 #Build the vnv toolkit library
 cd vnv 
+docker pull ${REPO_OWNER}/vnv:${REON} || echo "dne"
 docker build -f docker/Dockerfile --build-arg FROM_IMAGE=${REPO_OWNER}/gui:${REON} -t ${REPO_OWNER}/vnv:${REON} . 
+docker push ${REPO_OWNER}/vnv:${REON}
 cd .. 
 
 #Add all of the plugins
 cd plugins/performance
- docker build -f docker/Dockerfile --build-arg FROM_IMAGE=${REPO_OWNER}/vnv:${REON} -t ${REPO_OWNER}/vnvp:${REON} .  
+docker pull ${REPO_OWNER}/vnvp:${REON} || echo "dne"
+docker build -f docker/Dockerfile --build-arg FROM_IMAGE=${REPO_OWNER}/vnv:${REON} -t ${REPO_OWNER}/vnvp:${REON} .  
+docker push ${REPO_OWNER}/vnvp:${REON}
 cd ../.. 
 
 
